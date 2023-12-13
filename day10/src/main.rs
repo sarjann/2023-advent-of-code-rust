a08261a1be260a24e8df556346fa1a90c6048ee4
Sarjan Narwan <imsarj@gmail.com>
2023-12-13 16:22:50 +0000

    Day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -9,7 +9,7 @@ use utils::load;
 // F is a 90-degree bend connecting south and east.
 // . is ground; there is no pipe in this tile.
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Tile {
     Vertical,
     Horizontal,
@@ -56,12 +56,57 @@ fn get_generate_tile_array_start(data: &String) -> (Vec<Vec<Tile>>, (i64, i64)) 
 fn is_compatible(current_tile: &Tile, next_tile: &Tile, direction: &(i64, i64)) -> bool {
     match current_tile {
         Tile::Start => match next_tile {
-            Tile::Vertical => direction.0 == 0,
-            Tile::Horizontal => direction.1 == 0,
+            Tile::Vertical => direction.1 == 0,
+            Tile::Horizontal => direction.0 == 0,
             Tile::NorthEast => direction.0 == 1 || direction.1 == -1,
             Tile::NorthWest => direction.0 == 1 || direction.1 == 1,
             Tile::SouthWest => direction.0 == -1 || direction.1 == 1,
             Tile::SouthEast => direction.0 == -1 || direction.1 == -1,
+            _ => true,
+        },
+        Tile::Vertical => match next_tile {
+            Tile::NorthEast => direction.0 == 1,
+            Tile::NorthWest => direction.0 == 1,
+            Tile::SouthWest => direction.0 == -1,
+            Tile::SouthEast => direction.0 == -1,
+            Tile::Horizontal => false,
+            _ => true,
+        },
+        Tile::Horizontal => match next_tile {
+            Tile::SouthWest => direction.1 == 1,
+            Tile::SouthEast => direction.1 == -1,
+            Tile::NorthEast => direction.1 == -1,
+            Tile::NorthWest => direction.1 == 1,
+            Tile::Vertical => false,
+            _ => true,
+        },
+        Tile::NorthEast => match next_tile {
+            Tile::Vertical => direction.0 == -1,
+            Tile::Horizontal => direction.1 == 1,
+            Tile::NorthEast => false,
+            Tile::NorthWest => direction.1 == 1,
+            Tile::SouthEast => direction.0 == -1,
+            _ => true,
+        },
+        Tile::NorthWest => match next_tile {
+            Tile::Vertical => direction.0 == -1,
+            Tile::Horizontal => direction.1 == -1,
+            Tile::NorthEast => direction.1 == -1,
+            Tile::NorthWest => false,
+            _ => true,
+        },
+        Tile::SouthWest => match next_tile {
+            Tile::Vertical => direction.0 == 1,
+            Tile::Horizontal => direction.1 == -1,
+            Tile::SouthWest => false,
+            Tile::SouthEast => direction.1 == -1,
+            _ => true,
+        },
+        Tile::SouthEast => match next_tile {
+            Tile::Vertical => direction.0 == 1,
+            Tile::Horizontal => direction.1 == 1,
+            Tile::SouthWest => direction.1 == 1,
+            Tile::SouthEast => false,
             _ => true,
         },
         _ => true,
@@ -71,7 +116,6 @@ fn is_compatible(current_tile: &Tile, next_tile: &Tile, direction: &(i64, i64)) 
 fn recurse_path(
     tile_array: &Vec<Vec<Tile>>,
     visited: &mut HashSet<(i64, i64)>,
-    in_loop: &mut HashSet<(i64, i64)>,
     visited_nodes: &mut Vec<(i64, i64)>,
     current_node: (i64, i64),
     start_node: (i64, i64),
@@ -140,44 +184,124 @@ fn recurse_path(
         if recurse_path(
             &tile_array,
             visited,
-            in_loop,
             visited_nodes,
             new_node,
             start_node,
         ) {
             return true;
-        };
-        visited_nodes.pop();
+        } else {
+            visited_nodes.pop();
+        }
     }
     false
 }
 
 fn get_valid_path(
-    tile_array: Vec<Vec<Tile>>,
+    tile_array: &Vec<Vec<Tile>>,
     start: (i64, i64),
-) -> (Vec<(i64, i64)>, HashSet<(i64, i64)>, bool) {
+) -> (Vec<(i64, i64)>, bool) {
     let mut visited: HashSet<(i64, i64)> = HashSet::new();
-    let mut in_loop: HashSet<(i64, i64)> = HashSet::new();
     let mut visited_nodes: Vec<(i64, i64)> = vec![];
 
     let is_valid = recurse_path(
         &tile_array,
         &mut visited,
-        &mut in_loop,
         &mut visited_nodes,
         start,
         start,
     );
-    (visited_nodes, in_loop, is_valid)
+    println!("Visited Nodes: {:?}", visited_nodes.len());
+    println!("-----here---------------");
+    (visited_nodes, is_valid)
 }
 
+
+fn part2(tile_array: &Vec<Vec<Tile>>) -> i64 {
+    // Scan Line
+    let mut total = 0;
+    let mut valid_area = false;
+    let mut previous_tile = Tile::Ground;
+    for j in 0..tile_array.len() {
+        let mut count = 0;
+        for i in 0..tile_array[j].len() {
+            let current_tile = tile_array[j][i];
+            match previous_tile {
+                Tile::Vertical => {
+                    if valid_area {
+                        total += count;
+                        count = 0;
+                    }
+                    valid_area = !valid_area
+                },
+                Tile::SouthEast => {
+                    if valid_area {
+                        total += count;
+                        count = 0;
+                    }
+                        valid_area = !valid_area;
+                },
+                Tile::NorthEast=> {
+                    if valid_area {
+                        total += count;
+                        count = 0;
+                    }
+                        valid_area = !valid_area;
+                },
+                // Ugly hardcode
+                Tile::Start=> {
+                    if valid_area {
+                        total += count;
+                        count = 0;
+                    }
+                        valid_area = !valid_area;
+                },
+                _ => (),
+            }
+
+            match current_tile {
+                Tile::Ground => {
+                    count += 1;
+                    previous_tile = Tile::Ground;
+                },
+                Tile::Horizontal => {
+                    match previous_tile {
+                        // Case of NE/SE ------- NW/NE
+                        Tile::NorthEast => previous_tile = Tile::NorthEast,
+                        Tile::SouthEast => previous_tile = Tile::SouthEast,
+                        Tile::Start => previous_tile = Tile::Start,
+                        _ => {
+                            previous_tile = current_tile;
+                        },
+                    }
+                }
+                _ => previous_tile = current_tile,
+            }
+        }
+    }
+    total
+}
 fn main() {
     let data = load("input.txt");
     let (tile_array, start) = get_generate_tile_array_start(&data);
-    let (visited_nodes, in_loop, valid_path) = get_valid_path(tile_array, start);
+    let (visited_nodes, valid_path) = get_valid_path(&tile_array, start);
+
+    for j in 0..tile_array.len() {
+        print!("\n");
+        for i in 0..tile_array[j].len() {
+            let pos = (j as i64, i as i64);
+            if visited_nodes.contains(&pos) {
+                print!("x");
+            } else {
+                print!(".")
+            }
+        }
+    }
+    print!("\n");
+
     println!("Part 1");
     println!("Valid path: {}", valid_path);
-    println!("Visited nodes length: {}", visited_nodes.len());
-    println!("Half of visited nodes length: {}", visited_nodes.len() / 2);
+    println!("Visited/2 length: {}", visited_nodes.len() / 2);
     println!("Part 2");
+    let part2_result = part2(&tile_array);
+    println!("Result is:  {}", part2_result);
 }
